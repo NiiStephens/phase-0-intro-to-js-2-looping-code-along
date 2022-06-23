ecdd633d60757a59fb34365c8a4c568821c021af
Nii <bnstephens001@gmail.com>
2022-06-23 05:42:25 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = (name) => {
+    let newNamesArrMessage = [];
+    for (let i = 0; i < name.length; i++){
+        newNamesArrMessage.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return newNamesArrMessage;
+}
+
+const countDown = (num) => {
+    for (let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
